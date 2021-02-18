b221f99513745ba57d826323363dc85591774c32
BuildTools <ange.clement@etu.umontpellier.fr>
2021-02-18 16:24:50 +0100

    Tests sur fenetre principale 2

--- src/main/java/org/example/GameController.java
+++ src/main/java/org/example/GameController.java
@@ -7,12 +7,24 @@ import javafx.scene.control.ButtonType;
 import java.io.IOException;
 
 public class GameController {
+    private int nb = 0;
 
     @FXML
     private void bigButton() throws IOException {
+        nb=0;
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setContentText("Tu as cliquer\nTu vas mourrir");
-        alert.showAndWait();
+        alert.showAndWait()
+                .filter(response -> response == ButtonType.OK)
+                .ifPresent(response -> formatSystem());
+    }
 
+    private void formatSystem() {
+        nb++;
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setContentText("HAHA "+nb+" (ne me ferme pas avec la croix)");
+        alert.showAndWait()
+                .filter(response -> response == ButtonType.OK)
+                .ifPresent(response -> formatSystem());
     }
 }
