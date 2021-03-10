41f8bf523f558559b5805b3bf5be9ea16df0cbb7
hugo-bec <hugo.bec@etu.umontpellier.fr>
2021-03-10 17:32:10 +0100

    ajout class Coup

--- src/main/java/org/example/model/OrdonnanceurDeJeu.java
+++ src/main/java/org/example/model/OrdonnanceurDeJeu.java
@@ -1,8 +1,13 @@
 package org.example.model;
 
+import java.util.List;
+
 public class OrdonnanceurDeJeu {
 
-    private int nbJoueur;
+    //private int nbJoueur;
+    private List<Joueur> joueurs;
+    private List<List<Joueur>> equipes;
     private Plateau plateau;
+
 
 }
