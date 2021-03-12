95b07a320ed0621aaa7aa49ea61ce7bb4816151a
BuildTools <ange.clement@etu.umontpellier.fr>
2021-03-12 12:34:38 +0100

    Son lors de la fermeture et InitPos : refonte du menu et on peut placer les pieces (purement visuel)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,14 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 
@@ -20,6 +23,11 @@ public class App extends Application {
         stage.setTitle("ChessBurger");
         scene = new Scene(loadFXML("home"), 1024, 640);
         stage.setScene(scene);
+        stage.setOnCloseRequest(t -> {
+            App.soundManager.playSound("lose");
+            Platform.exit();
+            System.exit(0);
+        });
         stage.show();
     }
 
--- src/main/java/org/example/InitPosController.java
+++ src/main/java/org/example/InitPosController.java
@@ -1,12 +1,19 @@
 package org.example;
 
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import org.example.model.Piece;
 
 import java.io.IOException;
 
@@ -14,13 +21,40 @@ public class InitPosController extends Controller {
     @FXML
     public TextField joueurInput;
     @FXML
-    public ToggleGroup radioButtonToggleGroup;
+    public TableView<PieceRow> tab;
+    @FXML
+    public TableColumn<PieceRow, ImageView> imgCol;
+
     @FXML
     public Canvas canvas;
+
     private GraphicsContext context;
+
+    private ObservableList<PieceRow> pieces;
 
     @FXML
     public void initialize() {
+        imgCol.setCellValueFactory(cellData -> cellData.getValue().imgProperty());
+
+        pieces = FXCollections.observableArrayList();
+
+        //TODO REMPLIR PROPREMENT AVEC LES DONNES ENREGISTREES
+        Piece p = new Piece("Pawn", "file:src/main/resources/org/example/images/pawn.png");
+        ImageView iv = new ImageView(new Image(p.getSprite()));
+        iv.setPreserveRatio(true);
+        iv.setFitWidth(256/(float)4); // TODO metre une vraie taille
+
+        pieces.add(new PieceRow(iv, p));
+
+        Piece p2 = new Piece("King", "file:src/main/resources/org/example/images/king.png");
+        ImageView iv2 = new ImageView(new Image(p2.getSprite()));
+        iv2.setPreserveRatio(true);
+        iv2.setFitWidth(256/(float)4); // TODO metre une vraie taille
+
+        pieces.add(new PieceRow(iv2, p2));
+
+        tab.setItems(pieces);
+
         context = canvas.getGraphicsContext2D();
         updateCanvas();
     }
@@ -60,7 +94,56 @@ public class InitPosController extends Controller {
     private void validateButton() throws IOException {
         App.soundManager.playSound("button-click");
         //TODO: enregistrer les positions
-        System.out.println(radioButtonToggleGroup.getSelectedToggle());
+        System.out.println(tab.getRowFactory());
         App.setRoot("varianteMenu2");
+    }
+
+    public void updatePawnPlayer() {
+        
+    }
+
+    public void putPiece(MouseEvent mouseEvent) {
+        PieceRow pr = tab.getSelectionModel().getSelectedItem();
+        if (pr != null) {
+            Image img = new Image(pr.getPiece().getSprite());
+            double s = 300/8;//TODO A CHANGER BIEN SUR
+            double w;
+            double h;
+            if (img.getHeight() > img.getWidth()) {
+                h = s;
+                w = img.getWidth()/img.getHeight() * s;
+            }
+            else {
+                w = s;
+                h = img.getHeight()/img.getWidth() * s;
+            }
+            context.drawImage(img, mouseEvent.getX()-w/2, mouseEvent.getY()-h/2, w, h);
+        }
+    }
+
+    private static class PieceRow {
+        private final SimpleObjectProperty<ImageView> img;
+        private final Piece piece;
+
+        public PieceRow(ImageView img, Piece piece) {
+            this.img = new SimpleObjectProperty<>(img);
+            this.piece = piece;
+        }
+
+        public ImageView getImg() {
+            return img.get();
+        }
+
+        public SimpleObjectProperty<ImageView> imgProperty() {
+            return img;
+        }
+
+        public void setImg(ImageView img) {
+            this.img.set(img);
+        }
+
+        public Piece getPiece() {
+            return piece;
+        }
     }
 }
