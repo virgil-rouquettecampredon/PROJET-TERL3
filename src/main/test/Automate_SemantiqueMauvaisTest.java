acbb62878f9d3365c934636486b47db834859eb2
hugo-bec <hugo.bec@etu.umontpellier.fr>
2021-04-02 16:44:12 +0200

    Création classe test automate sémantique

---
+++ src/main/test/Automate_SemantiqueMauvaisTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class Automate_SemantiqueMauvaisTest {
+
+
+    //test etat terminal 306
+    @Test
+    public final void testCase306_026_Exception(){
+
+    }
+}
